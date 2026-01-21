a7bfeb265818d8d8771cf4ee5d8a1872fa8b67ab
realChanho <real2chanho@gmail.com>
2026-01-21 12:28:57 +0900

    문서배포

--- http/ch01/my-client/index.js
+++ http/ch01/my-client/index.js
@@ -1,1 +1,17 @@
-// todo 
+const http = require("http");
+const options = new URL("http://localhost:3000");
+
+const handler = (response) => {
+  const data = []
+
+  response.on("data", (chunk) => {
+    data.push(chunk.toString());
+  });
+
+  response.on("end", () => {
+    console.log(data.join(""));
+  });
+}
+
+const request = http.request(options, handler);
+request.end();
